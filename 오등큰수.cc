9a7e0409202c2fe868ad1f3402382963c11454ee
pmy <126655022+mooo10@users.noreply.github.com>
2024-03-22 16:28:52 +0900

    Create 오등큰수.cc

---
+++ 오등큰수.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n;
+
+    vector<int> sequence(n);
+    vector<int> F(n);
+    vector<int> NGF(n, -1);
+    vector<int> size(1000001, 0);
+
+    stack<int> s;
+
+    for(int i = 0; i < n; i++) {
+        cin >> m;
+        sequence[i] = m;
+        size[m]++;
+    }
+
+    for(int i = 0; i < n; i++) {
+        F[i] = size[sequence[i]];
+    }
+
+    for(int i = 0; i < n; i++) {
+        while(!s.empty() && F[s.top()] < F[i]) {
+            NGF[s.top()] = sequence[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+
+    for(int i = 0; i < NGF.size(); i++) {
+        cout << NGF[i];
+        if(i != NGF.size() - 1) {
+            cout << " ";
+        }
+    }   
+    return 0;
+}
